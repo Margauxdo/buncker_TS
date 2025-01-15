43f825aa2f41c51e6fdc9593dde85e96eec89369
Margaux Doisy <margauxdoisy@gmail.com>
2025-01-15 12:13:36 +0100

    fichier start.bat

--- src/main/java/example/BunckerTsApplication.java
+++ src/main/java/example/BunckerTsApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BunckerTsApplication {
 
-	public static void main(String[] args) {
+
+	public static void main(String[]) {
 		SpringApplication.run(BunckerTsApplication.class, args);
 	}
 }
---
+++ start_project.bat
@@ -0,0 +1,7 @@
+@echo off
+REM Déplacez-vous dans le répertoire contenant le fichier .jar
+cd /d C:\Users\marga\Documents\clients\thibaut_segard\buncker_TS\target
+
+REM Exécutez le fichier .jar
+java -jar java-0.0.1-SNAPSHOT.jar
+pause
